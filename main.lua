42433f43ec27db5a8f374212cf0b3fdc8460693c
Sivantha Paranavithana <madusham3@gmail.com>
2020-06-22 12:52:37 +0530

    Add Parellax Scrolling

--- main.lua
+++ main.lua
@@ -9,6 +9,13 @@ VIRTUAL_HEIGHT = 288
 local backgorund = love.graphics.newImage('background.png')
 local ground = love.graphics.newImage('ground.png')
 
+local backgroundScroll = 0
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+local BACKGROUND_LOOPING_POINT = 413
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
@@ -31,9 +38,14 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+end
+
 function love.draw()
     push:start()
-    love.graphics.draw(backgorund, 0, 0)
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    love.graphics.draw(backgorund, -backgroundScroll, 0)
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
     push:finish()
 end
